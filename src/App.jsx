b94dce6aa10104ed64274a191b0c7827ce52e5df
qmaoz <sergeygord1231@gmail.com>
2026-03-30 19:38:42 +0200

    Assignment #6 has been completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './components/Header.jsx';
 import Main from './components/Main.jsx';
 import Footer from './components/Footer.jsx';
 
 export default function App() {
-  const [products, setProducts] = useState([
-    { id: 1, name: 'Маргарита', price: 180, category: 'Піца', isSelected: false },
-    { id: 2, name: 'Пепероні', price: 220, category: 'Піца', isSelected: false },
-    { id: 3, name: 'Чотири сири', price: 250, category: 'Піца', isSelected: false },
-    { id: 4, name: 'Кока-Кола', price: 40, category: 'Напої', isSelected: false },
-    { id: 5, name: 'Апельсиновий сік', price: 55, category: 'Напої', isSelected: false },
-    // { id: 6, name: 'Чізбургер', price: 120, category: 'Бургери', isSelected: false },
-    // { id: 7, name: 'Дабл Біф Бургер', price: 190, category: 'Бургери', isSelected: false },
-    // { id: 8, name: 'Тірамісу', price: 95, category: 'Десерти', isSelected: false },
-    // { id: 9, name: 'Чізкейк', price: 110, category: 'Десерти', isSelected: false },
-    // { id: 10, name: 'Піца з морепродуктами', price: 310, category: 'Піца', isSelected: false },
-    // { id: 11, name: 'Холодний чай', price: 45, category: 'Напої', isSelected: false },
-    // { id: 12, name: 'Веган Бургер', price: 150, category: 'Бургери', isSelected: false }
-  ]);
+  const [products, setProducts] = useState(() => {
+    const stored = localStorage.getItem('products-data');
+
+    if (stored) {
+      try {
+        return JSON.parse(stored);
+      } catch (e) {
+        console.error("Помилка парсингу JSON", e);
+      }
+    }
+
+    return [
+      // Default products
+      { id: 1, name: 'Маргарита', price: 180, category: 'Піца', isSelected: false },
+      { id: 2, name: 'Пепероні', price: 220, category: 'Піца', isSelected: false },
+    ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('products-data', JSON.stringify(products));
+  }, [products]);
 
   return (
     <>
@@ -29,3 +36,16 @@ export default function App() {
     </>
   );
 }
+
+// [
+  // { id: 3, name: 'Чотири сири', price: 250, category: 'Піца', isSelected: false },
+  // { id: 4, name: 'Кока-Кола', price: 40, category: 'Напої', isSelected: false },
+  // { id: 5, name: 'Апельсиновий сік', price: 55, category: 'Напої', isSelected: false },
+  // { id: 6, name: 'Чізбургер', price: 120, category: 'Бургери', isSelected: false },
+  // { id: 7, name: 'Дабл Біф Бургер', price: 190, category: 'Бургери', isSelected: false },
+  // { id: 8, name: 'Тірамісу', price: 95, category: 'Десерти', isSelected: false },
+  // { id: 9, name: 'Чізкейк', price: 110, category: 'Десерти', isSelected: false },
+  // { id: 10, name: 'Піца з морепродуктами', price: 310, category: 'Піца', isSelected: false },
+  // { id: 11, name: 'Холодний чай', price: 45, category: 'Напої', isSelected: false },
+  // { id: 12, name: 'Веган Бургер', price: 150, category: 'Бургери', isSelected: false }
+// ]
